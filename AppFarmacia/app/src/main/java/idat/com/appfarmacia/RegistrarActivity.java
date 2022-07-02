fe55d19e4759b896ace888f295126810d47e42f2
RoggerK <at76324897@idat.edu.pe>
2022-07-02 11:03:22 -0500

    doc %80

--- AppFarmacia/app/src/main/java/idat/com/appfarmacia/RegistrarActivity.java
+++ AppFarmacia/app/src/main/java/idat/com/appfarmacia/RegistrarActivity.java
@@ -44,17 +44,47 @@ public class RegistrarActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * validarEmail(String correo):
+     *
+     * Se instancia un patron, para ver el valor
+     * de correo si este tiene al menos un @ y .
+     *
+     * Caso contrario botaria un mensaje de no
+     * formate de correo
+     *
+     * @param correo, de tipo String
+     * @return boolean
+     * */
+
     private boolean validarEmail(String correo) {
         Pattern pattern = Patterns.EMAIL_ADDRESS;
         boolean valor = pattern.matcher(correo).matches();
 
         if(!valor){
-            Toast.makeText(this, "No se tiene un forma del correo", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No se tiene un forma del correo",
+                            Toast.LENGTH_SHORT).show();
         }
 
         return valor;
     }
 
+    /**
+     * validarFecha(String fecha):
+     *
+     * Valida que sean numeros; sus longitudes
+     * de año igual a 4, mes y dia igual a 1 o 2.
+     * Por ultimo la excepcion de formato de fecha
+     * incorrecto se controla en conversion de numeros
+     *
+     * @author Curisinche Guia Kennedy
+     * @param fecha, foramto fecha aaaa/mm/dd o aaaa-mm-dd,
+     *               si la persona ingresa aaaa-mm/dd o
+     *               aaaa/mm-dd, sera valido, pero con
+     *               advertencia en sql que convertira en tipo
+     *               fecha aaaa-mm-dd
+     * @return boolean
+     * */
     private boolean validarFecha(String fecha) {
         try {
             String[] fc_nacimiento = fecha.split("[-/]+", 3);
@@ -65,23 +95,28 @@ public class RegistrarActivity extends AppCompatActivity {
                 }
 
                 if(!(fc_nacimiento[0].length() == 4)){
-                    Toast.makeText(this, "Longitud de año incorrecto", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Longitud de año incorrecto",
+                                    Toast.LENGTH_SHORT).show();
                     return false;
                 }
 
                 if(!(fc_nacimiento[1].length() >= 1 && fc_nacimiento[1].length() <= 2)
                         || !(fc_nacimiento[2].length() >= 1 && fc_nacimiento[2].length() <= 2)){
-                    Toast.makeText(this, "Longitud de mes o dia incorrecto", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Longitud de mes o dia incorrecto",
+                                    Toast.LENGTH_SHORT).show();
                     return false;
                 }
 
                 return true;
             } else {
-                Toast.makeText(this, "La fecha de nacimiento esta vacia", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "La fecha de nacimiento esta vacia",
+                                Toast.LENGTH_SHORT).show();
                 return false;
             }
         } catch (NumberFormatException e){
-            Toast.makeText(this, "La fecha de nacimiento contiene letras", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+            Toast.makeText(this, "La fecha de nacimiento contiene letras o el formato de fecha es incorrecto",
+                            Toast.LENGTH_SHORT).show();
             return false;
         }
     }
